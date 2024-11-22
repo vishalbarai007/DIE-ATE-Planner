422326b8db1be17f95b0e84afb43798ded92810a
vbytex <vishalbaraiofficial01@gmail.com>
2024-11-22 23:30:19 +0530

    Revert "Refactoring Folder Structure."
    This reverts commit 6c483627a9e127deebf3bfeedc333481fdb5fc48.

---
+++ FrontEnd/src/pages/Homepage.tsx
@@ -0,0 +1,17 @@
+import { CanvasRevealEffectDemo } from "../components/ReusableComponents/CardCanvasRevealEffect"
+
+const Homepage = () => {
+  return (
+    <div className="bg-zinc-900 h-screen w-full">
+        {/* <h1>
+            paxum 
+            Web money wmt
+            Web money wmz
+        </h1> */}
+        <CanvasRevealEffectDemo/>
+      
+    </div>
+  )
+}
+
+export default Homepage
