8a06dcb33a9c241ec4fd1e87fa9842dfc8d86453
Manishr345 <manishr030405@gmail.com>
2024-11-23 11:23:46 +0530

    Created an intro page using aceternity ui and splide slider

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Intro from "./pages/Intro"
+
+
+const App = () => {
+  return (
+    <div>
+      <Intro/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Intro.tsx
@@ -0,0 +1,9 @@
+import Carousel from '../components/AceternityComponents/main/Carousel'
+
+export default function Intro() {
+  return (
+    <div>
+      <Carousel/>
+    </div>
+  )
+}
