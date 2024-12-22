45240c268f56c352ca59c11c25ee213a8f46c6da
Manishr345 <manishr030405@gmail.com>
2024-12-22 18:18:08 +0530

    Fixed overflowing of title in ActionCards

--- FrontEnd/src/components/DeveloperDefineComponents/ActionCard.tsx
+++ FrontEnd/src/components/DeveloperDefineComponents/ActionCard.tsx
@@ -15,13 +15,13 @@ export function ActionCard({ icon: Icon, title, className }: ActionCardProps) {
 
   return (
     <div className={cn(
-      "flex items-center gap-2 sm:gap-4 p-3 sm:p-4 bg-white rounded-lg border hover:shadow-md transition-shadow cursor-pointer",
+      "flex items-center w-44 gap-2 sm:gap-2 p-1 sm:p-3 overflow-hidden bg-white rounded-lg border hover:shadow-md transition-shadow cursor-pointer",
       className
     )}>
       <div className="p-1 sm:p-2 rounded-lg bg-gray-50">
         <Icon className="w-4 h-4 sm:w-6 sm:h-6" />
       </div>
-      <span className={`text-sm sm:text-base font-medium ${contextTheme?.theme === 'light' ? 'text-black' : 'text-white'}`}>{title}</span>
+      <span className={`text-sm sm:text-sm font-medium ${contextTheme?.theme === 'light' ? 'text-black' : 'text-white'}`}>{title}</span>
     </div>
   )
 }
