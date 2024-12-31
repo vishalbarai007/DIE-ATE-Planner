7a7c4d2b35c183a825410d3f6d70b853ec5129b8
Raj Vishwakarma <rajrv6929@gmail.com>
2024-12-31 20:09:08 +0530

    Add Email&Pass Auth and Personal Info Storage

--- FrontEnd/firebase.ts
+++ FrontEnd/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from'firebase/auth';
-import 'firebase/firestore'; 
+import { getAuth, signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider, createUserWithEmailAndPassword, User } from'firebase/auth';
+import { getFirestore, collection, addDoc } from 'firebase/firestore'; 
 
 const firebaseConfig = {
     apiKey: "AIzaSyDRQhfdz24X8gjebMWCmnR15hqR2i7HpFY",
@@ -17,9 +17,45 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider();
+const db = getFirestore(firebaseApp);
 
+enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
 
-const handleGoogleLoginWithPopup = () => {
+interface UserData{
+  firstname: string,
+  lastname: string,
+  email: string,
+  password: string,
+  cpassword: string
+}
+
+interface introData{
+  age: number,
+  gender: Gender,
+  height: number,
+  weight: number,
+  issue: string,
+  goal: string
+}
+
+const signUp = (formData: UserData, introInfo: introData, redirect: Function) => {
+  createUserWithEmailAndPassword(auth, formData.email, formData.password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+    addPersonalData(introInfo, user.uid);
+    redirect();
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
+const handleGoogleLoginWithPopup = (introInfo: introData, redirect: Function) => {
     signInWithPopup(auth, provider)
   .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
@@ -33,6 +69,8 @@ const handleGoogleLoginWithPopup = () => {
     console.log(user); // REMOVE LATER
     // IdP data available using getAdditionalUserInfo(result)
     // ...
+    addPersonalData(introInfo, user.uid);
+    redirect();
   }).catch((error) => {
     // // Handle Errors here.
     // const errorCode = error.code;
@@ -46,7 +84,7 @@ const handleGoogleLoginWithPopup = () => {
   });
 }
 
-const handleGoogleLoginWithRedirect = () => {
+const handleGoogleLoginWithRedirect = (introInfo: introData, redirect: Function) => {
   signInWithRedirect(auth, provider);
   getRedirectResult(auth)
   .then((result) => {
@@ -62,6 +100,8 @@ const handleGoogleLoginWithRedirect = () => {
       console.log(user); // REMOVE LATER
       // IdP data available using getAdditionalUserInfo(result)
       // ...
+      addPersonalData(introInfo, user.uid);
+      redirect();
     }
   }).catch((error) => {
     // Handle Errors here.
@@ -74,8 +114,16 @@ const handleGoogleLoginWithRedirect = () => {
     console.log(error);
     // ...
   });
-
 }
 
-export { handleGoogleLoginWithPopup, handleGoogleLoginWithRedirect };
+const addPersonalData = async (data: introData, id: string) => {
+  try {
+    const docRef = await addDoc(collection(db, "personal-info"), { uid: id, ...data });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+export { handleGoogleLoginWithPopup, handleGoogleLoginWithRedirect, signUp };
 
